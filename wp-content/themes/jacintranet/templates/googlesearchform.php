fd280eabf78dfc5baed3eefca310a3f6fcd16cdd
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-10-12 11:31:10 +0100

    Change Google Search widget form action to submit to Google UK https encrypted page.

--- wp-content/themes/jacintranet/templates/googlesearchform.php
+++ wp-content/themes/jacintranet/templates/googlesearchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" action="http://www.google.com/search">
+<form role="search" method="get" action="https://www.google.co.uk/search">
   <label for="q">
     <input type="search" id="q" name="q" class="searchbox" />
     <input id="searchButton" class="SearchBtn" type="submit" name="searchButton" value="" />
